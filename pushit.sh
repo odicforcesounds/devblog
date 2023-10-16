0f8fbbc5fa4e38f1ae8f9eff9554d3f3aff3a77e
om <DaoExpression@users.noreply.github.com>
2023-10-16 03:32:17 +0100

    Make it Right for a better Ride

--- pushit.sh
+++ pushit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # IF you are using another domain to push your code... 
 
-# pkill -x ssh-agent 
-# sleep 1
-# echo "ssh-agents killed"
-# eval $(ssh-agent)
-# sleep 1 
-# echo "ssh-agent inicialized"
-# ssh-add ~/.ssh/github_odrsa
-# sleep 1
-# echo "ssh-key-id added"
+pkill -x ssh-agent 
+sleep 1
+echo "ssh-agents killed"
+eval $(ssh-agent)
+sleep 1 
+echo "ssh-agent inicialized"
+ssh-add ~/.ssh/github_odrsa
+sleep 1
+echo "ssh-key-id added"
 git add . 
 git commit -m "Make it Right for a better Ride"
 git push git@github.com:daoexpression/devblog
