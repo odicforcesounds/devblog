def5060e6000fcb3a9c2b72328a347e725db5095
om <om@devil.localhost.devil>
2025-07-25 03:10:22 +0100

    Make it Right for a better Ride

--- pushit.sh
+++ pushit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # IF you are using another domain to push your code... 
 git diff
-git pull git@github.com:odicforcesounds/devblog
 pkill -x ssh-agent 
 sleep 1
 echo "ssh-agents killed"
@@ -11,6 +10,7 @@ echo "ssh-agent inicialized"
 ssh-add ~/.ssh/github
 sleep 1
 echo "ssh-key-id added"
+git pull git@github.com:odicforcesounds/devblog
 git add . 
 git commit -m "Make it Right for a better Ride"
 git push git@github.com:odicforcesounds/devblog
