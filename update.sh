ddaf9a8258f62096a9f6b39cb5f732800d438a36
i00nsu <i00nsu@blocker.netcabo.pt>
2026-02-14 03:40:25 +0000

    Make it Right for a better Ride

--- update.sh
+++ update.sh
@@ -11,8 +11,3 @@ ssh-add ~/.ssh/github
 sleep 1
 echo "ssh-key-id added"
 git pull git@github.com:odicforcesounds/devblog
-git add . 
-git commit -m "Make it Right for a better Ride"
-git push git@github.com:odicforcesounds/devblog
-echo "Push completed"
-sleep 3
